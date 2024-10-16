bf7c5bc7adbcd6b0c42a17a9c0c325f06924cd32
Weverton Pires <30535993+magpires@users.noreply.github.com>
2024-10-16 20:41:55 -0300

    Adiciona Model Caixa

---
+++ Models/Caixa.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class Caixa
+    {
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+        public int Comprimento { get; set; }
+    }
+}
