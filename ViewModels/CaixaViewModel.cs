b76ea049f6fdb9deb2ce279f4b7309afff8f606c
Weverton Pires <30535993+magpires@users.noreply.github.com>
2024-10-16 00:57:07 -0300

    Adiciona ViewModels

---
+++ ViewModels/CaixaViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewModels
+{
+    public class CaixaViewModel
+    {
+        public string Caixa_id { get; set; } = string.Empty;
+        public List<string> Produtos { get; set; } = new List<string>();
+    }
+}
---
+++ ViewModels/PedidoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class PedidoViewModel
+    {
+        public int Pedido_id { get; set; }
+        public List<CaixaViewModel> Caixas { get; set; } = new List<CaixaViewModel>();
+    }
+}
